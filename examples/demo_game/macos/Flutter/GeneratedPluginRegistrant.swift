375e46afdc49f51c99c2318dbf39f3f3716e0cb6
6hostsRus <ghosts4hire666@icloud.com>
2025-09-01 13:32:10 -0500

    updated

--- examples/demo_game/macos/Flutter/GeneratedPluginRegistrant.swift
+++ examples/demo_game/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import amplitude_flutter
+import firebase_analytics
+import firebase_core
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AmplitudeFlutterPlugin.register(with: registry.registrar(forPlugin: "AmplitudeFlutterPlugin"))
+  FirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
 }
